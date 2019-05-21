491e0f93ddfd624061868506f09319ec422bdb76
DonnyB <donny.bereznak@bluesombrero.com>
2019-05-20 22:00:21 -0400

    added template engine

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,41 @@
 const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
 
-app.get('', (req, res) => {
-    res.send('Hello Express')
-})
+// define paths for express
+const pathDirectory = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
+
+// setup handlebars
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup dierectory to serve
+app.use(express.static(pathDirectory))
+
+app.get('', (req, res) => {
+    res.render('index', { 
+        title: 'Home Page',
+        header: 'Weather App',
+    name: 'Donny Bereznak'})
+})
 app.get('/help', (req, res) => {
-    res.send('Help Page');
+    res.render('index', { 
+        title: 'Help Page',
+        header: 'Get Help for the Weather App',
+        name: 'Donny Bereznak'})
 })
 
 app.get('/about', (req, res) => {
-    res.send('<h1>About Page</h1>');
+    res.render('index', { 
+        title: 'About Page',
+        header: 'Learn all about the Weather App',
+        name: 'Donny Bereznak'})
 })
 
 app.get('/weather', (req, res) => {
